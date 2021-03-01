d390ef689edc83bbc5ffdf7cd7dbc80a4ccc906d
msgumtow <71787320+msgumtow@users.noreply.github.com>
2021-03-01 11:22:14 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+/**
+ * <---Press the "A" button on the microbit simulator!
+ */
+/**
+ * <---Press the "A" button on the microbit simulator!
+ */
+/**
+ * ---------->
+ * 
+ * Place the two "show leds" blocks into each of the above blocks. 
+ * 
+ * You can drag them both by grabbing the top block.
+ * 
+ * On the simulator, watch the differences between how the heart animates. Then, go answer the questions in Google Classroom.
+ */
+/**
+ * <---Press the "A" button on the microbit simulator!
+ */
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+	
+})
