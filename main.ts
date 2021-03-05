a4d5e561703c510be5974ed99a91d116f99a481d
msgumtow <71787320+msgumtow@users.noreply.github.com>
2021-03-05 09:29:58 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,6 @@
  * <---Press the "A" button on the microbit simulator!
  */
 /**
- * <---Press the "A" button on the microbit simulator!
- */
-/**
  * ---------->
  * 
  * Place the two "show leds" blocks into each of the above blocks. 
@@ -14,10 +11,7 @@
  * On the simulator, watch the differences between how the heart animates.
  */
 /**
- * What were the differences between how the animation worked in the forever block, the on start, and the on button A pressed block? TYPE YOUR ANSWER HERE.
- */
-/**
- * <---Press the "A" button on the microbit simulator!
+ * What were the differences between how the animation worked in the forever block, the on start, and the on button A pressed block? TYPE YOUR ANSWER HERE
  */
 input.onButtonPressed(Button.A, function () {
 	
